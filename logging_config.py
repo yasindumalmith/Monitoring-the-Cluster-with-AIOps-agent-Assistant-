b1fa000ff5a0d2b591c30a476fdb46dcb6781ec8
yasindumalmith <yasindumalmith058@gmail.com>
2026-06-15 02:16:07 +0530

    implement structlog configuration for structured logging

---
+++ logging_config.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+import structlog
+
+
+def configure_logging():
+    logging.basicConfig(format="%(message)s", stream=sys.stdout, level=logging.INFO)
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.processors.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.JSONRenderer(),
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
+        logger_factory=structlog.PrintLoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
