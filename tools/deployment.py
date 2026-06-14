99efe48843e7ce37804c2f9285bdb374cd9b7e35
yasindumalmith <yasindumalmith058@gmail.com>
2026-06-15 01:46:53 +0530

    update for import cluster config

--- tools/deployment.py
+++ tools/deployment.py
@@ -1,6 +1,13 @@
 from kubernetes import client, config
 
-config.load_kube_config()
+def _load_config():
+    try:
+        config.load_incluster_config()
+    except config.ConfigException:
+        config.load_kube_config()
+
+
+_load_config()
 apps_v1 = client.AppsV1Api()
 
 
